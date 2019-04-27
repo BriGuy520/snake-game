997c22e57c69e25673cb91eb4711ee233bc179a4
Brian Falasz <brianfalasz@gmail.com>
2019-04-27 12:26:30 -0500

    added the boolean values that will check state

--- snake.js
+++ snake.js
@@ -8,7 +8,11 @@ const WEST = { x: -1, y: 0 };
 const pointEq = p1 => p2 => p1.x == p2.x && p1.y == p2.y;
 
 // Boolean
+const willEat = state => pointEq(nextHead(state))(state.apple);
+const willCrash = state => state.snake.find(pointEq(nextHead(state)));
+const validMove = move => state => state.moves[0].x + move.x != 0 || state.moves[0].y + move.y != 0;
 
+// Next values based on state 
 
 const rndPos = table => ({
   x: rnd(0)(table.cols - 1),
