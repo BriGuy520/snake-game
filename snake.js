80825acb464f8103c714d00753dae624f2d1f906
Brian Falasz <brianfalasz@gmail.com>
2019-04-27 12:21:48 -0500

    started working on snake file. Set up the pointer operations

--- snake.js
+++ snake.js
@@ -1,3 +1,20 @@
+
+const NORTH = { x: 0, y: -1 };
+const SOUTH = { x: 0, y: 1 };
+const EAST = { x: 1, y: 0};
+const WEST = { x: -1, y: 0 };
+
+// Point operations 
+const pointEq = p1 => p2 => p1.x == p2.x && p1.y == p2.y;
+
+// Boolean
+
+
+const rndPos = table => ({
+  x: rnd(0)(table.cols - 1),
+  y: rnd(0)(table.rows - 1)
+});
+
 const initialState = () => ({
   cols: 20,
   rows: 14,
