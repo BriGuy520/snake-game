777d454c61487b3707fd5f007d847a4ce664d47d
Brian Falasz <brianfalasz@gmail.com>
2019-04-27 12:33:11 -0500

    set up the moves in the game

--- snake.js
+++ snake.js
@@ -13,6 +13,19 @@ const willCrash = state => state.snake.find(pointEq(nextHead(state)));
 const validMove = move => state => state.moves[0].x + move.x != 0 || state.moves[0].y + move.y != 0;
 
 // Next values based on state 
+const nextMoves = state => state.moves.length > 1 ? dropFirst(state.moves) : state.moves;
+const nextApple = state => willEat(state) ? rndPos(state) : state.apple;
+const nextHead = state => state.snake.length == 0
+  ? { x: 2, y: 2 }
+  : {
+    x: mod(state.cols)(state[0].x + state.movesp[0].x),
+    y: mod(state.rows)(state.snake[0].y + state.moves[0].y)
+  }
+const nextSnake = state => willCrash(state)
+  ? []
+  : (willEat(state)
+  ? [nextHead(state)].concat(state.snake)
+  : [nextHead(state)].concat(dropLast(state.snake)));
 
 const rndPos = table => ({
   x: rnd(0)(table.cols - 1),
